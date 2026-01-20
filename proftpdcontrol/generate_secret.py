375ae1e41912056c40628d71d8d5b212201d8062
Alex Merkulov <arheops@gmail.com>
2026-01-20 14:54:13 -0600

    fix in secrets

--- proftpdcontrol/generate_secret.py
+++ proftpdcontrol/generate_secret.py
@@ -1,4 +1,4 @@
 import string
 import secrets
 allowed = string.ascii_letters + string.digits + '_-';
-print(''.join(secrets.choice(allowed) for _ in range(50))
+print(''.join(secrets.choice(allowed) for _ in range(50)))
